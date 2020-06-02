b9fb526f775e16c963af730485f55ecf41d46456
atharvakadam <atharvakadam@gmail.com>
2020-06-01 22:02:55 -0700

    Implemented the express-node.js backend

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 A backend server created using Node.js and Express.
+
+Remember to do `npm install` before you try to run it. This is because node_modules is included in the .gitignore file.
--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 // console.log('Hello World');
-const http = require('http');
+const express = require('express');
+const app = express();
 
 let notes = [
     {
@@ -21,11 +22,21 @@ let notes = [
       important: true
     }
   ]
-  const app = http.createServer((request, response) => {
-    response.writeHead(200, { 'Content-Type': 'application/json' })
-    response.end(JSON.stringify(notes))
-  })
+//   const app = http.createServer((request, response) => {
+//     response.writeHead(200, { 'Content-Type': 'application/json' })
+//     response.end(JSON.stringify(notes))
+//   })
 
-const port = 3001
-app.listen(port)
-console.log(`Server running on port ${port}`);
+app.get('/',(req,res) => {
+    res.send('<h1>Hello World !</h1>');
+});
+
+app.get('/api/notes',(req,res) => {
+    res.json(notes);
+})
+
+const PORT = 3001
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+})
+
