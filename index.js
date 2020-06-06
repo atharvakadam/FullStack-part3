7145de6c206564003736c83bb7d98453d9e2fadb
atharvakadam <atharvakadam@gmail.com>
2020-06-05 22:26:15 -0700

    Fixed put, post and delete

--- index.js
+++ index.js
@@ -59,11 +59,15 @@ app.get('/api/notes', (request, response) => {
     })
 });
 
-app.get('/api/notes/:id',(request,response) => {
+app.get('/api/notes/:id',(request,response, next) => {
     const id = request.params.id;
     Note.find({ _id : id}).then(notes => {
-        response.json(notes)
-      })
+        if (notes) {
+            response.json(notes)
+        } else {
+            response.status(404).end() 
+        }
+      }).catch(error => {next(error);});
 
     // const id = Number(request.params.id);
     // const note = notes.find(note => note.id === id)
@@ -75,11 +79,12 @@ app.get('/api/notes/:id',(request,response) => {
     // }
 })
 
-app.delete('/api/notes/:id',(request,response) => {
-    const id = Number(request.params.id);
-    notes = notes.filter(note => note.id !== id);
-
-    response.status(204).end();
+app.delete('/api/notes/:id',(request,response, next) => {
+    Note.findByIdAndRemove(request.params.id)
+    .then(result => {
+      response.status(204).end()
+    })
+    .catch(error => next(error))
 })
 
 const generateId = () => {
@@ -113,11 +118,38 @@ app.post('/api/notes', (request,response) => {
     // response.json(note);
 })
 
+app.put('/api/notes/:id', (request, response, next) => {
+    const body = request.body
+  
+    const note = {
+      content: body.content,
+      important: body.important,
+    }
+  
+    Note.findByIdAndUpdate(request.params.id, note, { new: true })
+      .then(updatedNote => {
+        response.json(updatedNote)
+      })
+      .catch(error => next(error))
+})
+
 const unknownEndpoint = (request, response) => {
     response.status(404).send({ error: 'unknown endpoint' })
-  }
+}
   
 app.use(unknownEndpoint)
+
+const errorHandler = (error, request, response, next) => {
+    console.error(error.message)
+  
+    if (error.name === 'CastError') {
+      return response.status(400).send({ error: 'malformatted id' })
+    } 
+  
+    next(error)
+}
+  
+app.use(errorHandler)
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
