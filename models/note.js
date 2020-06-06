4803f38e17f5e6b03f04142dc363505915eb9cc5
atharvakadam <atharvakadam@gmail.com>
2020-06-05 22:52:33 -0700

    uibuild

--- models/note.js
+++ models/note.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 mongoose.set('useFindAndModify', false)
 
-const url = String(process.env.MONGODB_URI)
+const url = process.env.MONGODB_URI + "";
 
 console.log('connecting to', url)
 console.log(typeof(url));
