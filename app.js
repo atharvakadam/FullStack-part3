0a72d16a5c8017fd9856552b34ee823b761d2fcf
atharvakadam <atharvakadam@gmail.com>
2020-06-06 21:03:46 -0700

    Implemeted initial testing and finished part4->a

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ logger.info('connecting to ', config.MONGODB_URI)
 mongoose.connect(config.MONGODB_URI, {useNewUrlParser: true, useUnifiedTopology: true}).then(() => {
     logger.info('Connected to MongoDB')
 }).catch((error) => {
-    logger.error('error connection to MongoDB', error.messaage)
+    logger.error('error connection to MongoDB', error.message)
 })
 
 app.use(cors())
