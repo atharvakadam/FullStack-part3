dab9be30fc05fad5b8b507bd72fcc342b47a6577
atharvakadam <atharvakadam@gmail.com>
2020-06-09 19:09:16 -0700

    Finished part4 -> c

--- controllers/notes.js
+++ controllers/notes.js
@@ -1,8 +1,9 @@
 const notesRouter = require('express').Router()
 const Note = require('../models/note')
+const User = require('../models/user')
 
 notesRouter.get('/', async (request, response) => {
-    const notes = await Note.find({})
+    const notes = await Note.find({}).populate('user',{username: 1, name:1})
     response.json(notes.map(note => note.toJSON()))
 
     // Note.find({}).then(notes => {
@@ -30,14 +31,21 @@ notesRouter.get('/:id', async (request, response) => {
 
 notesRouter.post('/', async (request, response) => {
     const body = request.body
+    console.log(body)
+    const user = await User.findById(body.userId)
+    console.log(user)
 
     const note = new Note({
         content: body.content,
         important: body.important || false,
-        date: new Date()
+        date: new Date(),
+        user: user._id
     })
 
     const savedNote = await note.save()
+    user.notes = user.notes.concat(savedNote._id)
+
+    await user.save()
     response.json(savedNote.toJSON())
     // try { 
         
